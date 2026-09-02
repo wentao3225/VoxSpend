20875a2bc7cadcfbf3473b0295e736fae8c3cdb3
Atao-Git <lanton2002@163.com>
2026-09-02 17:31:07 +0800

    fix: Windows 兼容，用 shell:true 启动 vite

--- voxspend-web/dev.cjs
+++ voxspend-web/dev.cjs
@@ -1,7 +1,9 @@
 const { spawn } = require('child_process');
+const path = require('path');
 
-const server = spawn('node', ['server.cjs'], { stdio: 'inherit' });
-const vite = spawn('npx', ['vite'], { stdio: 'inherit' });
+const viteBin = path.join(__dirname, 'node_modules', '.bin', 'vite');
+const server = spawn('node', ['server.cjs'], { stdio: 'inherit', cwd: __dirname });
+const vite = spawn(viteBin, [], { stdio: 'inherit', cwd: __dirname, shell: true });
 
 server.on('close', () => process.exit());
 vite.on('close', () => process.exit());
